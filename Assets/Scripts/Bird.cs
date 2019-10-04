3397045871cb08f04a0b09c479a523970b19f713
Thomas Nunes Botelho Barbosa - 51922570 <51922570@animaedu.intranet>
2019-10-04 10:40:23 -0300

    Criado prefab dos canos

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    public float speed = 1;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _rigidbody.velocity = Vector2.up * speed; 
+        }
+    }
+
+}
